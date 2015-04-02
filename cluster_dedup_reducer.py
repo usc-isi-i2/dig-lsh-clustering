5c500006a92f69b9d170f07420c83c6bf11f4007
dkapoor <dipsykapoor@gmail.com>
2015-04-02 17:21:00 +0000

    fix the cluster dedup reducer

--- cluster_dedup_reducer.py
+++ cluster_dedup_reducer.py
@@ -3,8 +3,6 @@
 import sys
 
 current_key = None
-current_score = 0
-key = None
 
 # input comes from STDIN
 for line in sys.stdin:
@@ -14,19 +12,12 @@ for line in sys.stdin:
     # parse the input we got from mapper.py
     (key, score) = line.split('\t', 1)
 
-
     # this IF-switch only works because Hadoop sorts map output
     # by key (here: word) before it is passed to the reducer
     if current_key == key:
-        current_score = score
+        continue
     else:
-        if current_key:
-            # write result to STDOUT
-            (key1, key2) = current_key.split("$$$$", 1)
-            print '%s\t%s\t%s' % (key1, key2, current_score)
-        current_count = score
+        (key1, key2) = key.split("$$$$", 1)
+        print '%s\t%s\t%s' % (key1, key2, score)
         current_key = key
 
-# do not forget to output the last word if needed!
-if current_key == key:
-    print '%s\t%s' % (current_key, current_score)
