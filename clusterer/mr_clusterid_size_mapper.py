ae616cb2fdee17dfbefcfd2e84551817aaa4621d
Dipsy Kapoor <dipsykapoor@gmail.com>
2015-04-16 11:00:38 -0700

    Extra scripts to compute cluster statistics

--- clusterer/mr_clusterid_size_mapper.py
+++ clusterer/mr_clusterid_size_mapper.py
@@ -1,11 +1,12 @@
-#!/usr/bin/env python
-
-import sys
+ import sys
+import hashlib
 
 # input comes from STDIN (standard input)
 for line in sys.stdin:
     # remove leading and trailing whitespace
     line = line.strip()
     if len(line) > 0:
-        print line
+        (lsh, cluster_id, cluster_count, minhash) = line.split("\t", 3)
+        print "%s\t%s" % cluster_id, cluster_count
+exit(0)
 
