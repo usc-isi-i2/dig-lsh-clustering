ffd8ccdb7f6eb5367388bccb7e8d21b4afdc9111
d3admin <d3admin@tintinisiedu.local>
2015-04-29 17:12:29 -0700

    modified generateTokens script

--- tokenizer/generateTokens.py
+++ tokenizer/generateTokens.py
@@ -3,37 +3,16 @@ import sys
 import ngram
 import unicodedata
 import re
+import json
+import codecs
 
-class TokenPreprocessor:
-
-    STOP_WORDS = ["a", "an", "and", "are", "as", "at", "be", "but", "by",
-"for", "if", "in", "into", "is", "it",
-"no", "not", "of", "on", "or", "such",
-"that", "the", "their", "then", "there", "these",
-"they", "this", "to", "was", "will", "with", "-", ";", ",", "_", "+", "/", "\\"]
-
-
-    def getPreprocessedTokens(self,text,compute_n_gram_characters,compute_n_gram_words,n):
-        #removes the stop words
-        tokens = list(self.tokenize_input(text))
-        if compute_n_gram_words:
-            return ["".join(j) for j in zip(*[tokens[i:] for i in range(n)])]
-        if compute_n_gram_characters:
-            ngramObject = ngram.NGram(N=n)
-            tokenizedText=text.replace(" ","")
-            ngram_char_tokens = list(ngramObject.split(tokenizedText))
-            ## remove first n-1 and last n-1 tokens as they are not complete they have $ signs
-            return ngram_char_tokens[n-1:(len(ngram_char_tokens)-(n-1))] if len(text) > n else text
-        else:
-            return list(self.tokenize_input(text))
-
-    def tokenize_input(self,input):
-        tokens = str(input).split()
+def tokenize_input(input):
+        tokens = input.split()
         for token in tokens:
-            if not token in TokenPreprocessor.STOP_WORDS:
+            if not token in STOP_WORDS:
                 yield token
 
-    def write_tokens(self,tokens, sep):
+def write_tokens(tokens, sep):
         outStr = ""
         usep = ""
         if isinstance(tokens,str):
@@ -44,26 +23,66 @@ class TokenPreprocessor:
                 usep = sep
         return outStr
 
-    def convert_to_LowerCase(self,text):
-        return text.lower()
 
-    def convert_UTF8_toAscii(self,text):
-        return unicodedata.normalize('NFKD', "".join(text)).encode('ascii','ignore')
+def getNGrams(text,type,n):
+        #removes the stop words
+        tokens = list(tokenize_input(text))
+        if(type == "word"):
+            if(len(tokens) > n):
+                return ["".join(j) for j in zip(*[tokens[i:] for i in range(n)])]
+            else:
+            #returns the word directly if n is greater than number of words
+                a=[]
+                a.append(text)
+                return a
+        if(type == "character"):
+            ngramObject = ngram.NGram(N=n)
+            ngram_char_tokens = list(ngramObject.split(text))
+            ## remove first n-1 and last n-1 tokens as they are not complete they have $ signs
+            if len(text) > n:
+                return ngram_char_tokens[n-1:(len(ngram_char_tokens)-(n-1))]
+            else:
+                tokens = []
+                tokens.append(text)
+                return tokens
+        else:
+            return list(tokenize_input(text))
 
-    def remove_special_characters(self,text):
-        return re.sub('[^\w\s]', '', str(text))
+# does the preprocessing takes fieldvalue as input-
+# does regex evaluations specified in configuration file, converts to utf8, lowercase
+#returns the tokens character or word
+def doPreprocessing(dict,fieldvalue):
+    try:
+        if('replacements' in dict):
+            for regex in dict['replacements']:
+                text = re.sub(regex['regex'], regex['replacement'], fieldvalue,flags=re.UNICODE)
+            if('filters' in dict):
+                for filter in dict['filters']:
+                    if(('latinfilter' in filter) and (filter['latinfilter'] == True)):
+                        text = unicodedata.normalize('NFKD', "".join(text))
+                    if(('lowercase' in filter) and (filter['lowercase'] == True)):
+                        text = text.lower()
+                    if(('uppercase' in filter) and (filter['uppercase'] == True)):
+                        text = text.upper()
+                    if(('removemultiplespaces' in filter) and (filter['removemultiplespaces']==True)):
+                        text = ' '.join(text.split())
 
-    def replace_All_White(self,text):
-        return ' '.join(text.split())
+            if('tokenization' in dict):
+                for param in dict['tokenization']:
+                    if('size' in param):
+                        size = param["size"]
+                    if('type' in param):
+                        type = param["type"]
+            return getNGrams(text,type,size)
+    except KeyError,e:
+        print "Keyerror " % str(e)
+
 
 def parse_args():
     global inputFilename
     global outputFilename
     global separator
-    global n_gram_words
-    global n_gram_characters
-    global n_gram_size
-
+    global configurationFilename
     for arg_idx, arg in enumerate(sys.argv):
         if arg == "--input":
             inputFilename = sys.argv[arg_idx+1]
@@ -74,55 +93,46 @@ def parse_args():
         if arg == "--separator":
             separator = sys.argv[arg_idx+1]
             continue
-        if arg == "--computengramwords":
-            n_gram_words = sys.argv[arg_idx+1]
-            continue
-        if arg == "--computengramcharacters":
-            n_gram_characters = sys.argv[arg_idx+1]
-            continue
-        if arg=="--ngramsize":
-            n_gram_size = sys.argv[arg_idx+1]
-            continue
-
-
-inputFilename = None
-outputFilename = None
-n_gram_words = None
-n_gram_characters = True
-n_gram_size = 3
-separator = "\t"
+        if arg == "--config":
+            configurationFilename = sys.argv[arg_idx+1]
 
 def die():
     print "Please input the required parameters"
-    print "Usage: GenerateTokens.py --input <input filename> --output <output filename> [--separator <sep=\\t>] [--computengramcharacters <True or False>] [--computengramwords <True or False>] " \
-          "[--ngramsize <size>]"
+    print "Usage: generateTokensNew.py --input <input filename> --config <configuration filename> --output <output filename> [--separator <sep=\\t>]"
     exit(1)
 
-
-# check if this is main because when this this class is called from other module the following code should not get executed
-
-if __name__ == '__main__':
-
-    parse_args()
-
-  #  if len(sys.argv) < 3:
-   #     die()
-
-    file = open(inputFilename,'r')
-    outputFile = open(outputFilename,'w')
-    tokenPreprocessor = TokenPreprocessor();
-    for line in file:
-        lineParts = list(line.strip().split(separator))
-        preprocessedText = tokenPreprocessor.replace_All_White(lineParts[0])
-        preprocessedText = tokenPreprocessor.remove_special_characters(lineParts[0])
-        ngram_tokens = TokenPreprocessor().getPreprocessedTokens(preprocessedText,n_gram_characters,n_gram_words,int(n_gram_size))
-        outputFile.write(preprocessedText + "\t" +TokenPreprocessor().write_tokens(ngram_tokens,"\t") + "\n")
-
-    file.close()
-    outputFile.close()
+inputFilename = "SampleFiles/city-state-country-US.csv"
+configurationFilename = "configFile_for_Tokenizer.json"
+outputFilename = "city-state-country-US-6grams.csv"
+separator = "\t"
 
 
+parse_args()
 
+if len(sys.argv) < 3:
+     die()
 
+file = codecs.open(inputFilename,'r',"utf-8")
+outputFile = codecs.open(outputFilename,'w','utf-8')
+configurationFile = open(configurationFilename)
+data = json.load(configurationFile)
+STOP_WORDS = data['stopwords']
 
+for line in file:
+    lineParts = list(line.strip().split(separator))
+    key = ""
+    usep=""
+    ngram_tokens = []
+    for index,fieldvalue in enumerate(lineParts):
+        key = key + fieldvalue
+        key = re.sub('[^\w\s]', '', key)
+        key = ''.join(key.split())
+        if (data['config'][index]['defaultConfig'] == True):
+            ngram_tokens = ngram_tokens + doPreprocessing(data['defaultConfig']['analyzer'],fieldvalue)
+        else:
+            ngram_tokens = ngram_tokens + doPreprocessing(data['config'][index]['analyzer'],fieldvalue)
 
+    outputFile.write(key.lower() + separator +write_tokens(ngram_tokens,separator) + "\n")
+
+file.close()
+outputFile.close()
