1fe5eb0ec8c2914aa474b38ba4af38f5619a50c0
Dipsy Kapoor <dipsykapoor@gmail.com>
2015-04-30 12:41:21 -0700

    condensed config file. Includes setting like Elastic Search

--- tokenizer/generateTokens.py
+++ tokenizer/generateTokens.py
@@ -7,78 +7,91 @@ import json
 import codecs
 
 
+def tokenize_input(input):
+    tokens = input.split()
+    for token in tokens:
+        yield token
 
 #returns the tokens removing the stop words
-def tokenize_input(input):
-        tokens = input.split()
-        for token in tokens:
-            if not token in STOP_WORDS:
-                yield token
+def tokenize_input_stopwords(input, stop_words):
+    tokens = input.split()
+    for token in tokens:
+        if not token in stop_words:
+            yield token
 
 def write_tokens(tokens, sep):
-        outStr = ""
-        usep = ""
-        if isinstance(tokens,str):
-            return tokens
-        elif isinstance(tokens,list):
-            for token in tokens:
-                outStr = outStr + usep + token
-                usep = sep
-        return outStr
+    outStr = ""
+    usep = ""
+    if isinstance(tokens,str):
+        return tokens
+    elif isinstance(tokens,list):
+        for token in tokens:
+            outStr = outStr + usep + token
+            usep = sep
+    return outStr
 
 
 def getNGrams(text,type,n):
-        #removes the stop words
-        tokens = list(tokenize_input(text))
-        if(type == "word"):
-            if(len(tokens) > n):
-                return ["".join(j) for j in zip(*[tokens[i:] for i in range(n)])]
-            else:
-            #returns the word directly if n is greater than number of words
-                a=[]
-                a.append(text)
-                return a
-        if(type == "character"):
-            ngramObject = ngram.NGram(N=n)
-            ngram_char_tokens = list(ngramObject.split(text))
-            ## remove first n-1 and last n-1 tokens as they are not complete they have $ signs
-            if len(text) > n:
-                return ngram_char_tokens[n-1:(len(ngram_char_tokens)-(n-1))]
-            else:
-                a = []
-                a.append(text)
-                return a
+    #removes the stop words
+    tokens = list(tokenize_input(text))
+    if(type == "word"):
+        if(len(tokens) > n):
+            return ["".join(j) for j in zip(*[tokens[i:] for i in range(n)])]
         else:
-            return list(tokenize_input(text))
+            #returns the word directly if n is greater than number of words
+            a=[]
+            a.append(text)
+            return a
+    if(type == "character"):
+        ngramObject = ngram.NGram(N=n)
+        ngram_char_tokens = list(ngramObject.split(text))
+        ## remove first n-1 and last n-1 tokens as they are not complete they have $ signs
+        if len(text) > n:
+            return ngram_char_tokens[n-1:(len(ngram_char_tokens)-(n-1))]
+        else:
+            a = []
+            a.append(text)
+            return a
+    else:
+        return list(tokenize_input(text))
 
 # does the preprocessing takes fieldvalue as input-
 # does regex evaluations specified in configuration file, converts to utf8, lowercase
 #returns the tokens character or word
-def doPreprocessing(dict,fieldvalue):
-    try:
-        if('replacements' in dict):
-            for regex in dict['replacements']:
-                text = re.sub(regex['regex'], regex['replacement'], fieldvalue,flags=re.UNICODE)
-            if('filters' in dict):
-                for filter in dict['filters']:
-                    if(('latinfilter' in filter) and (filter['latinfilter'] == True)):
-                        text = unicodedata.normalize('NFKD', "".join(text))
-                    if(('lowercase' in filter) and (filter['lowercase'] == True)):
-                        text = text.lower()
-                    if(('uppercase' in filter) and (filter['uppercase'] == True)):
-                        text = text.upper()
-                    if(('removemultiplespaces' in filter) and (filter['removemultiplespaces']==True)):
-                        text = ' '.join(text.split())
+def doPreprocessing(text, analyzer, settings):
+    if analyzer.has_key("replacements"):
+        for replacement in analyzer["replacements"]:
+            text = re.sub(replacement['regex'], replacement['replacement'], text,flags=re.UNICODE)
 
-            if('tokenization' in dict):
-                for param in dict['tokenization']:
-                    if('size' in param):
-                        size = param["size"]
-                    if('type' in param):
-                        type = param["type"]
-            return getNGrams(text,type,size)
-    except KeyError,e:
-        print "Keyerror " % str(e)
+    if analyzer.has_key("filters"):
+        for filter in analyzer["filters"]:
+            if filter == "lowercase":
+                text = text.lower()
+            elif filter == "uppercase":
+                text = text.upper()
+            elif filter == "latin":
+                text = unicodedata.normalize('NFKD', "".join(text))
+            else:
+                filter_settings = settings[filter]
+                if filter_settings["type"] == "stop":
+                    words = filter_settings["words"]
+                    tokens = tokenize_input_stopwords(text, words)
+                    text = " ".join(tokens)
+
+    tokens = []
+    if analyzer.has_key("tokenizers"):
+        for tokenizer in analyzer["tokenizers"]:
+            if tokenizer == "whitespace":
+                tokens = tokenize_input(text)
+            else:
+                tokenizer_setting = settings[tokenizer]
+                if tokenizer_setting["type"] == "character_ngram":
+                    size = int(tokenizer_setting["size"])
+                    tokens = getNGrams(text, "character", size)
+                elif tokenizer_setting["type"] == "word_ngram":
+                    size = int(tokenizer_setting["size"])
+                    tokens = getNGrams(text, "word", size)
+    return tokens
 
 
 def parse_args():
@@ -101,7 +114,7 @@ def parse_args():
 
 def die():
     print "Please input the required parameters"
-    print "Usage: generateTokensNew.py --input <input filename> --config <configuration filename> --output <output filename> [--separator <sep=\\t>]"
+    print "Usage: generateTokens.py --input <input filename> --config <configuration filename> --output <output filename> [--separator <sep=\\t>]"
     exit(1)
 
 
@@ -114,29 +127,29 @@ separator = "\t"
 parse_args()
 
 if len(sys.argv) < 3:
-     die()
+    die()
 
 file = codecs.open(inputFilename,'r',"utf-8")
 outputFile = codecs.open(outputFilename,'w','utf-8')
 configurationFile = open(configurationFilename)
-data = json.load(configurationFile)
-STOP_WORDS = data['stopwords']
+config = json.load(configurationFile)
+settings = config["settings"]
 
 for line in file:
     lineParts = list(line.strip().split(separator))
-    key = ""
-    usep=""
-    ngram_tokens = []
-    for index,fieldvalue in enumerate(lineParts):
-        key = key + fieldvalue
-        key = re.sub('[^\w\s]', '', key)
-        key = ''.join(key.split())
-        if (data['config'][index]['defaultConfig'] == True):
-            ngram_tokens = ngram_tokens + doPreprocessing(data['defaultConfig']['analyzer'],fieldvalue)
+    key = lineParts[0]
+    lineParts = lineParts[1:]
+    tokens = []
+    for index, fieldvalue in enumerate(lineParts):
+        if(config["fieldConfig"].has_key(str(index))):
+            analyzer = config["fieldConfig"][str(index)]["analyzer"]
         else:
-            ngram_tokens = ngram_tokens + doPreprocessing(data['config'][index]['analyzer'],fieldvalue)
+            analyzer = config["defaultConfig"]["analyzer"]
 
-    outputFile.write(key.lower() + separator +write_tokens(ngram_tokens,separator) + "\n")
+        field_tokens = doPreprocessing(fieldvalue, analyzer, settings)
+        tokens.extend(field_tokens)
+    outputFile.write(key + separator + write_tokens(tokens, separator) + "\n")
 
+print "Done tokenizing"
 file.close()
 outputFile.close()
