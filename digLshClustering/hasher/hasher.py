324ee2070f2653163b1b93104a66aa20b9000237
rajagopal067 <raj.bhojan@gmail.com>
2016-03-04 11:32:32 -0800

    added saveminhash option

--- digLshClustering/hasher/hasher.py
+++ digLshClustering/hasher/hasher.py
@@ -12,6 +12,9 @@ class Hasher:
     def __init__(self, **options):
         self.signer = MinHashSignature(options.get("numHashes",100))
         self.hasher = LSH(options.get("numHashes",100),options.get("numItemsInBand",10), None)
+        self.save_min_hash = options.get("saveMinhashes",False)
+        print 'in hashing'
+        print options.get("numHashes",100),options.get("numsItemsInBand",10),options.get("saveMinhashes",False)
 
 
     def perform(self,rdd):
@@ -28,6 +31,8 @@ class Hasher:
             #print min_hash_sig
             if min_hash_sig is not None:
                 lsh_sig = list(self.hasher.hash(min_hash_sig))
+                if self.save_min_hash is False:
+                    min_hash_sig = None
                 for lsh_val in lsh_sig:
                     yield lsh_val, (key, min_hash_sig)
 
@@ -44,11 +49,14 @@ if __name__ == "__main__":
                       help="number of minhashes", default=100)
     parser.add_option("-b", "--numItemsInBand", dest="numItemsInBand", type="int",
                       help="number of items in each band", default=10)
+    parser.add_option("-s", "--saveMinhashes", action="store_true",
+                      dest="saveMinhashes", default=False, help="save minhashes")
 
     (c_options, args) = parser.parse_args()
     print "Got options:", c_options
     inputFilename = args[0]
     outputFilename = args[1]
+
 
     hasher = Hasher(c_options)
     rdd = sc.sequenceFile(inputFilename).mapValues(lambda x: json.loads(x))
